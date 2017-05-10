e8cd23fd4470d11bbec92a4f42a96f787e521d71
Robert Petri <rpetri@captechconsulting.com>
2017-05-10 09:33:49 -0400

    changed output

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var app = require('express')();
 var express = require ('express');
 var http = require('http').Server(app);
+var ip = require("ip");
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World!'+ip.address());
 });
 
 http.listen(8080, function(){
