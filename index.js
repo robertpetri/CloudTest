e05f275331b24f11cf589ac0890a115023b84837
Robert Petri <rpetri@captechconsulting.com>
2017-05-10 08:20:40 -0400

    changed the index.js

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-var http = require('http');
-var express = require('express');
-var app = express();
-
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
-
-
-
-http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+var app = require('express')();
+var express = require ('express');
+var http = require('http').Server(app);
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+http.listen(8080, function(){
+  console.log('listening on *:3000');
+
+});
+
