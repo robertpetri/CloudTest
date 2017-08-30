5546f637877915cea95aded40561a34cbb2db569
Robert Petri <rpetri@captechconsulting.com>
2017-08-30 14:27:44 -0400

    Change

--- index.js
+++ index.js
@@ -3,9 +3,31 @@ var express = require ('express');
 var http = require('http').Server(app);
 var ip = require("ip");
 
+//MySQL (Deltas)
+// var mysql = require('mysql');
+// var connection = mysql.createConnection({
+//   //host     : process.env.RDS_HOSTNAME,
+//   host : 'podia.cvyyr7fp78yy.us-west-2.rds.amazonaws.com',
+//   //user     : process.env.RDS_USERNAME,
+//   user : 'robert',
+//   //password : process.env.RDS_PASSWORD,
+//   password : 'asianpat69',
+//   //port     : process.env.RDS_PORT
+//   port : 3306
+// });
+
 app.get('/', function (req, res) {
-  res.send('Hello World!       '+ip.address());
+  res.send('Hello World! THOMAS      '+ip.address());
 });
+// app.get('/test', function(req,res){
+// connection.query('SELECT 1', function (error, results, fields) {
+//   if (error) throw error;
+//   else{
+//   	console.log("connected");
+//   }
+//   // connected! 
+// });
+// });
 
 http.listen(8080, function(){
   console.log('listening on *:8080');
